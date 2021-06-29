a594ef833bb6a0e9e3f97ca446423074af0a82ae
Julian Hu <julianhy@gmail.com>
2021-06-29 22:05:45 +0800

    bad linked

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+use grrs::type_of;
+
+mod utils;
+
+/// example:
+/// grrs ./ --pattern test1
+#[derive(Debug, StructOpt)]
+#[structopt(name="grrs example", about="An example for command line app.")]
+struct Cli {
+    /// the pattern to look for
+    #[structopt(short, long)]
+    pattern: String,
+
+    /// the path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+
+    #[structopt(short, long, parse(from_os_str), default_value="./")]
+    output: PathBuf,
+}
+
+fn main() {
+    let args = Cli::from_args();
+
+    println!("Hello, world! for {:?}", args);
+    println!("type of args is: {:?}", type_of(args));
+}
