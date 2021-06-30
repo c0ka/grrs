cdfe97b065bef264fe0d7c76b407ebb109f0bb00
Julian Hu <julianhy@gmail.com>
2021-06-30 10:48:26 +0800

    3.4 IntoIter

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,15 @@
 use std::mem;
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item>{
+        self.0.pop()
+    }
+}
+
 #[allow(dead_code)]
 pub struct List<T> {
     head: Link<T>,
@@ -44,6 +54,10 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -55,6 +69,9 @@ impl<T> Drop for List<T> {
         }
     }
 }
+
+
+
 
 #[cfg(test)]
 mod test {
@@ -98,6 +115,18 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();   
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 
 }
