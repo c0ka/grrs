e48eb37d881afe77e6af19aa9135f114fee26704
Julian Hu <julianhy@gmail.com>
2021-07-01 15:46:45 +0800

    3.5 Iter

--- src/utils.rs
+++ src/utils.rs
@@ -10,13 +10,33 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
-#[allow(dead_code)]
+/// Iter of the stack holding a "next" attribute pointing to the next value should be returned.
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            // the alternative:
+            // self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+/// linked stack with head attribute pointing to it's top.
+/// because the stack maybe be null, so we should use Option
 pub struct List<T> {
     head: Link<T>,
 }
 
-type Link<T > = Option<Box<Node<T>>>;
+type Link<T> = Option<Box<Node<T>>>;
 
+/// the node of stack, including a value and a pointer to the below one
 struct Node<T> {
     elem: T,
     next: Link<T>,
@@ -29,6 +49,8 @@ impl<T> List<T> {
     }
 
     pub fn peek(&self) -> Option<&T> {
+        // creat a new Option<&Box<Node>>, and map the it's content with the ref of node's elem
+        // which is an Option<&elem>. notice the auto deref of the outer Box.
         self.head.as_ref().map(|node| {
             &node.elem
         })
@@ -43,6 +65,7 @@ impl<T> List<T> {
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
             elem: elem, 
+            // takes the value out and replace the origin with a None
             next: self.head.take(),
         });
         self.head = Some(new_node);
@@ -57,6 +80,12 @@ impl<T> List<T> {
 
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        // head is an Option wrap the Box pointing to Node, as_deref() will deref the Option's content
+        // that's deref the Box and return &Node.
+        Iter { next: self.head.as_deref() }
     }
 }
 
@@ -109,8 +138,8 @@ mod test {
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
 
-        list.peek_mut().map(|value| {
-            *value = 42
+        list.peek_mut().map(|elem| {
+            *elem = 42
         });
 
         assert_eq!(list.peek(), Some(&42));
@@ -128,6 +157,22 @@ mod test {
         assert_eq!(iter.next(), Some(1));
         assert_eq!(iter.next(), None);
     }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter1 = list.iter();
+        let mut iter2 = list.iter();
+        assert_eq!(iter1.next(), Some(&3));
+        assert_eq!(iter2.next(), Some(&3));
+        assert_eq!(iter1.next(), Some(&2));
+        assert_eq!(iter1.next(), Some(&1));
+        assert_eq!(iter2.next(), Some(&2));
+        assert_eq!(iter2.next(), Some(&1));
+    }
+
 
 }
 
